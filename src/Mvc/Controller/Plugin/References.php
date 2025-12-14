1caa0b2a498c393edf156f68e0abc3e514264042
Robin Leroy <egg.robin.leroy@gmail.com>
2025-12-14 19:20:39 +0100

    Revert "meow"
    This reverts commit d94b351849c60c4324a02899a55d7f123a35357f.

--- src/Mvc/Controller/Plugin/References.php
+++ src/Mvc/Controller/Plugin/References.php
@@ -1930,6 +1930,7 @@ class References extends AbstractPlugin
                 : "ELSE UPPER(LEFT($value, 1)) END)";
             $qb->addSelect($getInitial . " AS" . ($this->optionsCurrent['_initialName'] ?? 'initial'));
         } else {
+            $beverysad ($this->optionsCurrent['initial']);
             if (in_array($type, ['resource_classes', 'resource_templates', 'item_sets', 'resource_titles'])
                 && $this->optionsCurrent['initial']
             ) {
