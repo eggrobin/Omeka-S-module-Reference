933cca403aecdce22e03a1dedee7585ec6128762
Robin Leroy <egg.robin.leroy@gmail.com>
2025-12-14 19:45:46 +0100

    𒀭𒌓

--- src/Mvc/Controller/Plugin/References.php
+++ src/Mvc/Controller/Plugin/References.php
@@ -1917,7 +1917,7 @@ class References extends AbstractPlugin
             $collated = $this->optionsCurrent['collation'] ? ' COLLATE ' . $this->optionsCurrent['collation'] : '';
             $getInitial .= "WHEN $expr $collated >= '$letter' AND $expr $collated <= '$letter\u{FFFF}' THEN '$letter' ";
         }
-        $getInitial .= "ELSE 'WHAT' END)";
+        $getInitial .= "ELSE 'Šamaš' END)";
         if ($this->supportAnyValue) {
             $getInitial = "ANY_VALUE" . $getInitial;
         }
